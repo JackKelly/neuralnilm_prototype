389eedf1cf87b3319df31ff23bbb874af8f2f394
Jack Kelly <jack-list@xlk.org.uk>
2015-01-30 16:52:30 +0000

    using fdiff.  Doesn't work very well.

--- scripts/lstm.py
+++ scripts/lstm.py
@@ -1,12 +1,13 @@
+from __future__ import division
 import numpy as np
 import theano
 import theano.tensor as T
 import lasagne
 theano.config.compute_test_value = 'raise'
 # Sequence length
-LENGTH = 100
+LENGTH = 200
 # Number of units in the hidden (recurrent) layer
-N_HIDDEN = 4
+N_HIDDEN = 15
 # Number of training sequences in each batch
 N_BATCH = 30
 # Delay used to generate artificial training data
@@ -14,20 +15,19 @@ DELAY = 2
 # SGD learning rate
 LEARNING_RATE = 1e-1
 # Number of iterations to train the net
-N_ITERATIONS = 100
-
+N_ITERATIONS = 1000
 
 def gen_single_appliance(length, power, on_duration, min_off_duration=20):
-    appliance_power = np.zeros(shape=(length))
+    appliance_power = np.zeros(shape=(length+1))
     i = 0
-    while i < length:
+    while i < length+1:
         if np.random.binomial(n=1, p=0.2):
-            end = min(i + on_duration, length)
+            end = min(i + on_duration, length+1)
             appliance_power[i:end] = power
             i += on_duration + min_off_duration
         else:
             i += 1
-    return appliance_power
+    return np.diff(appliance_power)
 
 def gen_batches_of_single_appliance(length, n_batch, *args, **kwargs):
     batches = np.zeros(shape=(n_batch, length, 1))
@@ -58,8 +58,10 @@ def gen_data(length=LENGTH, n_batch=N_BATCH, n_appliances=2,
     X = y.copy()
     for power, on_duration in zip(appliance_powers, appliance_on_durations)[1:]:
         X += gen_batches_of_single_appliance(length, n_batch, power=power, on_duration=on_duration)
+
+    max_power = np.sum(appliance_powers)
     
-    return X, y
+    return X / max_power, y / max_power
 
 # Generate a "validation" sequence whose cost we will periodically compute
 X_val, y_val = gen_data()
@@ -107,11 +109,15 @@ all_params = lasagne.layers.get_all_params(l_out)
 updates = lasagne.updates.nesterov_momentum(cost, all_params, LEARNING_RATE)
 # Theano functions for training, getting output, and computing cost
 train = theano.function([input, target_output],
-                        cost, updates=updates, on_unused_input='warn')
+                        cost, updates=updates, on_unused_input='warn',
+                        allow_input_downcast=True)
 y_pred = theano.function(
-    [input], l_out.get_output(input), on_unused_input='warn')
+    [input], l_out.get_output(input), on_unused_input='warn',
+    allow_input_downcast=True)
+
 compute_cost = theano.function(
-    [input, target_output], cost, on_unused_input='warn')
+    [input, target_output], cost, on_unused_input='warn',
+    allow_input_downcast=True)
 
 # Train the net
 costs = np.zeros(N_ITERATIONS)
@@ -129,3 +135,13 @@ plt.plot(costs)
 plt.xlabel('Iteration')
 plt.ylabel('Cost')
 plt.show()
+
+def plot_estimates():
+    y_predictions = y_pred(X)
+    ax = plt.gca()
+    ax.plot(y_predictions[0,:,0], label='estimate')
+    ax.plot(y[0,:,0], label='ground truth')
+    ax.plot(X[0,:,0], label='aggregate')
+    ax.legend()
+    plt.show()
+
