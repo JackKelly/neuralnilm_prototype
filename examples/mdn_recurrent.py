8c1ef15b3ee896131b4376d3c86f2060824755c3
Jack Kelly <jack-list@xlk.org.uk>
2015-03-23 14:08:23 +0000

    plain RNN can do a decent job on this task

--- examples/mdn_recurrent.py
+++ examples/mdn_recurrent.py
@@ -3,26 +3,29 @@ import numpy as np
 import theano
 import theano.tensor as T
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 import lasagne
 from lasagne.utils import floatX
-from lasagne.layers import InputLayer, RecurrentLayer, ReshapeLayer
+from lasagne.layers import InputLayer, RecurrentLayer, ReshapeLayer, DenseLayer
 from lasagne.nonlinearities import tanh
 from lasagne.init import Normal
+from lasagne.objectives import Objective
 
 from neuralnilm.layers import MixtureDensityLayer
 from neuralnilm.objectives import mdn_nll
 
 # Number of units in the hidden (recurrent) layer
 N_HIDDEN_LAYERS = 2
-N_UNITS_PER_LAYER = 25
+N_UNITS_PER_LAYER = 10
 N_COMPONENTS = 2
 # Number of training sequences in each batch
 N_SEQ_PER_BATCH = 2
 SEQ_LENGTH = 256
 SHAPE = (N_SEQ_PER_BATCH, SEQ_LENGTH, 1)
 # SGD learning rate
-LEARNING_RATE = 0.00005
+#LEARNING_RATE = 0.00005
+LEARNING_RATE = 0.001
 # Number of iterations to train the net
 N_ITERATIONS = 5000
 
@@ -40,22 +43,25 @@ def gen_data():
             Target sequence
     '''
     NOISE_MAGNITUDE = 0.1
+    PULSE_WIDTH = 10
+    START = 100
+    STOP = 250
+    ON = 1.0
+    OFF = 0.0
     def noise():
         return floatX(np.random.uniform(
             low=-NOISE_MAGNITUDE, high=NOISE_MAGNITUDE, size=SHAPE))
 
-    t = np.zeros(shape=SHAPE, dtype=np.float32) - 1.0 + NOISE_MAGNITUDE
-    X = np.zeros(shape=SHAPE, dtype=np.float32) - 1.0 + NOISE_MAGNITUDE
-
-    X[:,100:150,:] = 1.0 - NOISE_MAGNITUDE
+    t = np.zeros(shape=SHAPE, dtype=np.float32) + OFF
+    X = np.zeros(shape=SHAPE, dtype=np.float32) + OFF
+    X[:,START:STOP,:] = ON
 
     for batch_i in range(N_SEQ_PER_BATCH):
         if np.random.binomial(n=1, p=0.5):
-#            X[batch_i,50:100,0] = np.linspace(-0.9, 0.9, 50)
-            X[batch_i,50:100,0] = 0.3
-            t[batch_i,:,0] = X[batch_i,:,0].copy()
-        # else:
-        #     X[:,100:150,:] = 0.3
+            for pulse_start in range(START, STOP, PULSE_WIDTH*2):
+                pulse_end = pulse_start + PULSE_WIDTH
+                X[batch_i, pulse_start:pulse_end, 0] = OFF
+            t[batch_i, :, 0] = X[batch_i, :, 0].copy()
     X += noise()
     return X, t
 
@@ -71,8 +77,11 @@ for i in range(N_HIDDEN_LAYERS):
         gradient_steps=100)
     layers.append(layer)
 layers.append(ReshapeLayer(layers[-1], (N_SEQ_PER_BATCH * SEQ_LENGTH, N_UNITS_PER_LAYER)))
-layers.append(MixtureDensityLayer(
-    layers[-1], num_units=t_val.shape[-1], num_components=N_COMPONENTS))
+# layers.append(MixtureDensityLayer(
+#     layers[-1], num_units=t_val.shape[-1], num_components=N_COMPONENTS))
+layers.append(DenseLayer(
+    layers[-1], num_units=1, nonlinearity=None, 
+    W=Normal(std=1.0/np.sqrt(N_UNITS_PER_LAYER))))
 # layers.append(ReshapeLayer(layers[-1], SHAPE))
 
 print("Total parameters: {}".format(
@@ -86,9 +95,10 @@ t = T.matrix('t')
 X.tag.test_value = floatX(np.random.rand(*SHAPE))
 t.tag.test_value = floatX(np.random.rand(N_SEQ_PER_BATCH * SEQ_LENGTH, 1))
 
-loss_func = mdn_nll
-y = layers[-1].get_output(X)
-loss = loss_func(y, t)
+#loss_func = mdn_nll
+# y = layers[-1].get_output(X)
+objective = Objective(layers[-1])
+loss = objective.get_loss(X, t)
 
 all_params = lasagne.layers.get_all_params(layers[-1])
 updates = lasagne.updates.nesterov_momentum(loss, all_params, LEARNING_RATE)
@@ -119,9 +129,22 @@ ax = plt.gca()
 ax.plot(X_val[batch_i,:,0], linewidth=1)
 ax.plot(t_val[batch_i*SEQ_LENGTH:(batch_i+1)*SEQ_LENGTH,0], linewidth=1)
 x = range(SEQ_LENGTH)
-for i in range(N_COMPONENTS):
-    ax.scatter(x, y[0][batch_i*SEQ_LENGTH:(batch_i+1)*SEQ_LENGTH,0,i], s=y[2][:,i] * 5)
+ax.plot(y[batch_i*SEQ_LENGTH:(batch_i+1)*SEQ_LENGTH,0], linewidth=1)
+# for i in range(N_COMPONENTS):
+#     ax.scatter(x, y[0][batch_i*SEQ_LENGTH:(batch_i+1)*SEQ_LENGTH,0,i], s=y[2][:,i] * 5)
 plt.show()
 
-def gmm_pdf(mu, sigma, mixing):
-    pass
+def gmm_pdf(theta, x):
+    """
+    Parameters
+    ----------
+    theta : tuple of (mu, sigma, mixing)
+    """
+    pdf = None
+    for mu, sigma, mixing in zip(theta):
+        norm_pdf = mixing * norm.pdf(x=x, loc=mu, scale=sigma)
+        if pdf is None:
+            pdf = norm_pdf
+        else:
+            pdf += norm_pdf
+    return pdf
