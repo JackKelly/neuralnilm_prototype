c76458cc50b54dcb993d4633a9efb3a36fc32def
Jack Kelly <jack-list@xlk.org.uk>
2015-03-24 17:39:03 +0000

    runs and doesn't NaN but is still VERY slow

--- neuralnilm/objectives.py
+++ neuralnilm/objectives.py
@@ -1,7 +1,10 @@
 from __future__ import print_function, division
+import numpy as np
+
+import theano
 from theano.ifelse import ifelse
 import theano.tensor as T
-import numpy as np
+
 from lasagne.utils import floatX
 
 
@@ -18,6 +21,7 @@ def scaled_cost(x, t, loss_func=lambda x, t: (x - t) ** 2):
     return (above_thresh_mean + below_thresh_mean) / 2.0
 
 
+TWO_PI = getattr(np, theano.config.floatX)(2 * np.pi)
 def mdn_nll(theta, t):
     """Computes the mean of negative log likelihood for P(t|theta) for
     Mixture Density Network output layers.
@@ -39,6 +43,25 @@ def mdn_nll(theta, t):
     mu.name     = 'mu'
     sigma.name  = 'sigma'
     mixing.name = 'mixing'
+    num_outputs = t.shape[1]
+
+    sq_error = (t.dimshuffle(0, 1, 'x') - mu) ** 2
+    exponent = -0.5 * T.inv(sigma) * T.sum(sq_error, axis=1)
+    normalizer = TWO_PI * sigma
+    exponent = exponent + T.log(mixing) - (num_outputs * .5) * T.log(normalizer)
+    log_gauss = log_sum_exp(exponent, axis=1)
+    return -T.mean(log_gauss)
+
+
+def log_sum_exp(x, axis=None):
+    # from https://github.com/Theano/Theano/issues/1563
+    x_max = T.max(x, axis=axis, keepdims=True)
+    x_mod = x - x_max
+    summed = T.sum(T.exp(x_mod), axis=axis, keepdims=True)
+    return T.log(summed) + x_max
+
+
+def old():
     pdf = gmm_pdf(t.dimshuffle(0, 1, 'x'), mu, sigma, mixing)
     pdf = pdf.reshape(shape=t.shape)
     log_pdf = T.log(pdf)
@@ -57,7 +80,24 @@ def gmm_pdf(x, mu, sigma, mixing):
     return T.batched_tensordot(normal_pdfs, mixing, axes=1)
 
 
-def LogSumExp(x, axis=None):
-    # from https://github.com/Theano/Theano/issues/1563
-    x_max = T.max(x, axis=axis, keepdims=True)
-    return T.log(T.sum(T.exp(x - x_max), axis=axis, keepdims=True)) + x_max
+
+def NLL(mu, sigma, mixing, y):
+    """Computes the mean of negative log likelihood for P(y|x)
+    
+    y = T.matrix('y') # (minibatch_size, output_size)
+    mu = T.tensor3('mu') # (minibatch_size, output_size, n_components)
+    sigma = T.matrix('sigma') # (minibatch_size, n_components)
+    mixing = T.matrix('mixing') # (minibatch_size, n_components)
+
+    """
+    
+    # multivariate Gaussian
+    exponent = -0.5 * T.inv(sigma) * T.sum((y.dimshuffle(0,1,'x') - mu)**2, axis=1)
+    normalizer = (2 * np.pi * sigma)
+    exponent = exponent + T.log(mixing) - (y.shape[1]*.5)*T.log(normalizer) 
+    max_exponent = T.max(exponent ,axis=1, keepdims=True)
+    mod_exponent = exponent - max_exponent
+    gauss_mix = T.sum(T.exp(mod_exponent),axis=1)
+    log_gauss = max_exponent + T.log(gauss_mix) 
+    res = -T.mean(log_gauss)
+    return res
