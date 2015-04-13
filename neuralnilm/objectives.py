957d84d782b64609490356164eb7f996f9414ce4
Jack Kelly <jack-list@xlk.org.uk>
2015-04-13 17:13:03 +0100

    getting there

--- neuralnilm/objectives.py
+++ neuralnilm/objectives.py
@@ -40,31 +40,30 @@ def ignore_inactive(x, t, loss_func=mse, seq_length=None):
     return error_only_active.mean()
 
 
-def scaled_cost_ignore_inactive(x, t, loss_func=mse, 
-                                seq_length=None):
+def scaled_cost_ignore_inactive(x, t, loss_func=mse, seq_length=None):
     error = loss_func(x, t)
-    if seq_length is not None:
+    if seq_length is None:
+        seq_length = t.shape[1]
+    else:
         n_seq_per_batch = t.shape[0] // seq_length
         shape = (n_seq_per_batch, seq_length, t.shape[-1])
         error = error.reshape(shape)
         t = t.reshape(shape)
 
-    active_seqs = (t > THRESHOLD).sum(axis=1) > 0
-    active_seqs = active_seqs.nonzero()
-    error_only_active = error.dimshuffle(0, 2, 1)[active_seqs]
-    t_only_active = t.dimshuffle(0, 2, 1)[active_seqs]
+    error = error.eval()
+    n_seq_per_batch = error.shape[1]
+#    n_seq_per_batch = 16 # CHANGE THIS!
+    for seq_i in range(n_seq_per_batch):
+        elements_above_thresh = t[seq_i, :, 0] > THRESHOLD
+        n_above_thresh = elements_above_thresh.sum()
+        if n_above_thresh == 0:
+            error[seq_i, :, :] = 0
+        else:
+            error[seq_i, elements_above_thresh, 0] *= 0.5 / n_above_thresh
+            n_below_thresh = seq_length - n_above_thresh
+            error[seq_i, -elements_above_thresh, 0] *= 0.5 / n_below_thresh
 
-    def mask_and_mean_error(mask):
-        masked_error = error_only_active[mask.nonzero()]
-        mean = masked_error.mean()
-        mean = ifelse(T.isnan(mean), 0.0, mean)
-        return mean
-
-    mask = t_only_active > THRESHOLD
-    above_thresh_mean = mask_and_mean_error(mask)
-    below_thresh_mean = mask_and_mean_error(-mask)
-    cost = (above_thresh_mean + below_thresh_mean) / 2.
-    return cost
+    return error.sum()
 
 
 TWO_PI = sfloatX(2 * np.pi)
