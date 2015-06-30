4cbe87ac142fec9bbb1fcdd08c10309f5d83b46c
Jack Kelly <jack-list@xlk.org.uk>
2015-06-30 10:50:55 +0100

    can now turn rectangles into a matrix and then into a 1d series.  But not very fast...

--- neuralnilm/disaggregate.py
+++ neuralnilm/disaggregate.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function
 import numpy as np
 from collections import namedtuple
 import csv
-from os.path import join
+from os.path import join, expanduser
 
 from neuralnilm.source import standardise
 
@@ -107,16 +107,120 @@ def disaggregate_start_stop_end(mains, net, stride=1):
     return rectangles
 
 
+def rectangle_filename(output_i, path=''):
+    """
+    Parameters
+    ----------
+    output_i : int
+    path : string
+
+    Returns
+    -------
+    full_filename : string
+    """
+    path = expanduser(path)
+    base_filename = 'disag_rectangles_output{:d}.csv'.format(output_i)
+    full_filename = join(path, base_filename)
+    return full_filename
+
+
 def save_rectangles(rectangles, path=''):
+    """
+    Parameters
+    ----------
+    rectangles : dict
+        Output from `disaggregate_start_stop_end()`
+    path : string
+    """
     for output_i, rects in rectangles.iteritems():
-        filename = 'disag_rectangles_output{:d}.csv'.format(output_i)
-        filename = join(path, filename)
+        filename = rectangle_filename(output_i, path)
         print("Saving", filename)
         with open(filename, 'wb') as f:
             writer = csv.writer(f)
             writer.writerow(Rectangle._fields)
             writer.writerows(rects)
         print("Done saving", filename)
+
+
+def load_rectangles(path=''):
+    rectangles = {}
+    for output_i in range(256):
+        filename = rectangle_filename(output_i, path)
+        try:
+            f = open(filename, 'rb')
+        except IOError:
+            if output_i == 0:
+                raise IOError(
+                    "No rectangle CSV files found in {}".format(path))
+            else:
+                break
+        rects = []
+        reader = csv.reader(f)
+        next(reader)  # skip header
+        for row in reader:
+            left = int(row[0])
+            right = int(row[1])
+            height = float(row[2])
+            rect = Rectangle(left=left, right=right, height=height)
+            rects.append(rect)
+        f.close()
+        rectangles[output_i] = rects
+
+    return rectangles
+
+
+def rectangles_to_matrix(rectangles, max_appliance_power):
+    """
+    Parameters
+    ----------
+    rectangles : list of Rectangles
+        Value of dict output from `disaggregate_start_stop_end()`
+    max_appliance_power : int or float
+        Watts
+
+    Returns
+    -------
+    matrix : 2D numpy.ndarray
+        Normalised to [0, 1]
+    """
+    n_samples = rectangles[-1].right
+    matrix = np.zeros(shape=(max_appliance_power, n_samples))
+    for rect in rectangles:
+        height = int(round(rect.height * max_appliance_power))
+        matrix[:height, rect.left:rect.right] += 1
+    matrix /= matrix.max()
+    return matrix
+
+
+def plot_rectangles_matrix(matrix):
+    import matplotlib.pyplot as plt
+    plt.imshow(matrix, aspect='auto', interpolation='none', origin='lower')
+    plt.show()
+
+
+def rectangles_matrix_to_vector(matrix, overlap_threshold=0.5):
+    """
+    Parameters
+    ----------
+    matrix : 2D numpy.ndarray
+        Output from `rectangles_to_matrix`
+    overlap_threshold : float, [0, 1]
+
+    Returns
+    -------
+    vector : 1D numpy.ndarray
+        Watts
+    """
+    n_samples = matrix.shape[1]
+    vector = np.zeros(n_samples)
+    matrix[matrix < overlap_threshold] = 0
+    for i in range(n_samples):
+        row_indicies = np.nonzero(matrix[:, i])[0]
+        if row_indicies.size > 0:
+            power = np.max(row_indicies)
+            vector[i] = power
+
+    return vector
 
 """
 Emacs variables
