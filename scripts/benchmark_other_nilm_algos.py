c6d362eebf6db46b9e39f075572170c3763d8b00
Jack Kelly <jack-list@xlk.org.uk>
2015-07-05 17:49:22 +0100

    benchmarking with FHMM and CO is done :)

--- scripts/benchmark_other_nilm_algos.py
+++ scripts/benchmark_other_nilm_algos.py
@@ -3,12 +3,13 @@ import numpy as np
 import pandas as pd
 from os.path import join
 import nilmtk
-from nilmtk.disaggregate import CombinatorialOptimisation
+from nilmtk.disaggregate import CombinatorialOptimisation, fhmm_exact
 from neuralnilm.metrics import run_metrics, across_all_appliances
 import yaml
 
 ukdale = nilmtk.DataSet('/data/mine/vadeec/merged/ukdale.h5')
-ukdale.set_window("2013-04-12", "2014-12-10")
+#ukdale.set_window("2013-04-12", "2014-12-10")
+ukdale.set_window("2013-04-12", "2013-05-12")
 elec = ukdale.buildings[1].elec
 
 BASE_DIRECTORY = '/home/jack/experiments/neuralnilm/figures/'
@@ -35,41 +36,81 @@ for appliance in APPLIANCES:
     meters.append(meter)
 meters = nilmtk.MeterGroup(meters)
 
-# TRAIN
-disag = CombinatorialOptimisation()
-disag.train(meters)
-
-# TEST
 mains = np.load(join(BASE_DIRECTORY, 'e545a/mains.npy'))
 mains = pd.DataFrame(mains)
-appliance_powers = disag.disaggregate_chunk(mains)
-
-# METRICS
 scores = {}
-scores['CO'] = {}
-aggregate_predictions = None
-for i, df in appliance_powers.iteritems():
-    appliance = disag.model[i]['training_metadata'].dominant_appliance()
-    appliance_type = appliance.identifier.type
-    y_pred = df.values
-    y_true_fname = join(
-        BASE_DIRECTORY, EXPERIMENT_DIRECTORIES[appliance_type],
-        'targets.npy')
-    y_true = np.load(y_true_fname)
-    n = min(len(y_true), len(y_pred))
-    y_true = y_true[:n]
-    y_pred = y_pred[:n]
-    scores['CO'][appliance_type] = run_metrics(y_true, y_pred, mains.values)
 
-    if aggregate_predictions is None:
-        aggregate_predictions = y_pred
-    else:
-        n_agg = min(len(aggregate_predictions), len(y_pred))
-        aggregate_predictions = aggregate_predictions[:n_agg]
-        aggregate_predictions += y_pred[:n_agg]
+def co():
+    # TRAIN
+    disag = CombinatorialOptimisation()
+    disag.train(meters)
 
-scores['CO'] = across_all_appliances(
-    scores['CO'], mains, aggregate_predictions)
+    # TEST
+    appliance_powers = disag.disaggregate_chunk(mains)
+
+    # METRICS
+    scores['CO'] = {}
+    aggregate_predictions = None
+    for i, df in appliance_powers.iteritems():
+        appliance = disag.model[i]['training_metadata'].dominant_appliance()
+        appliance_type = appliance.identifier.type
+        y_pred = df.values
+        y_true_fname = join(
+            BASE_DIRECTORY, EXPERIMENT_DIRECTORIES[appliance_type],
+            'targets.npy')
+        y_true = np.load(y_true_fname)
+        n = min(len(y_true), len(y_pred))
+        y_true = y_true[:n]
+        y_pred = y_pred[:n]
+        scores['CO'][appliance_type] = run_metrics(y_true, y_pred, mains.values)
+
+        if aggregate_predictions is None:
+            aggregate_predictions = y_pred
+        else:
+            n_agg = min(len(aggregate_predictions), len(y_pred))
+            aggregate_predictions = aggregate_predictions[:n_agg]
+            aggregate_predictions += y_pred[:n_agg]
+
+    scores['CO'] = across_all_appliances(
+        scores['CO'], mains, aggregate_predictions)
+
+
+def fhmm():
+    # TRAIN
+    disag = fhmm_exact.FHMM()
+    disag.train(meters)
+
+    # TEST
+    appliance_powers = disag.disaggregate_chunk(mains)
+
+    # METRICS
+    scores['FHMM'] = {}
+    aggregate_predictions = None
+    for meter, df in appliance_powers.iteritems():
+        appliance = meter.dominant_appliance()
+        appliance_type = appliance.identifier.type
+        y_pred = df.values
+        y_true_fname = join(
+            BASE_DIRECTORY, EXPERIMENT_DIRECTORIES[appliance_type],
+            'targets.npy')
+        y_true = np.load(y_true_fname)
+        n = min(len(y_true), len(y_pred))
+        y_true = y_true[:n]
+        y_pred = y_pred[:n]
+        scores['FHMM'][appliance_type] = run_metrics(
+            y_true, y_pred, mains.values)
+
+        if aggregate_predictions is None:
+            aggregate_predictions = y_pred
+        else:
+            n_agg = min(len(aggregate_predictions), len(y_pred))
+            aggregate_predictions = aggregate_predictions[:n_agg]
+            aggregate_predictions += y_pred[:n_agg]
+
+    scores['FHMM'] = across_all_appliances(
+        scores['FHMM'], mains, aggregate_predictions)
+
+
 print()
 print(yaml.dump(scores, default_flow_style=False))
 
