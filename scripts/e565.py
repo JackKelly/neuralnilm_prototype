b908483c2cca144f5e067b398d72f419c2e2c57a
Jack Kelly <jack-list@xlk.org.uk>
2015-07-17 06:26:58 +0100

    further tweaks to 565a

--- scripts/e565.py
+++ scripts/e565.py
@@ -134,77 +134,77 @@ def exp_a(name):
     global multi_source
 
     SEQ_LENGTH = 2048
-    N_SEQ_PER_BATCH = 16
+    N_SEQ_PER_BATCH = 8
 
-    real_appliance_source1 = RealApplianceSource(
-        logger=logger,
-        filename=UKDALE_FILENAME,
-        appliances=[
-            TARGET_APPLIANCE,
-            ['fridge freezer', 'fridge', 'freezer'],
-            'dish washer',
-            'kettle',
-            ['washer dryer', 'washing machine']
-        ],
-        max_appliance_powers=[MAX_TARGET_POWER, 300, 2500, 2600, 2400],
-        on_power_thresholds=[ON_POWER_THRESHOLD] + [10] * 4,
-        min_on_durations=[MIN_ON_DURATION, 60, 1800, 12, 1800],
-        min_off_durations=[MIN_OFF_DURATION, 12, 1800, 12, 600],
-        divide_input_by_max_input_power=False,
-        window_per_building=WINDOW_PER_BUILDING,
-        seq_length=SEQ_LENGTH,
-        output_one_appliance=True,
-        train_buildings=TRAIN_BUILDINGS,
-        validation_buildings=VALIDATION_BUILDINGS,
-        n_seq_per_batch=N_SEQ_PER_BATCH,
-        skip_probability=0.75,
-        skip_probability_for_first_appliance=SKIP_PROBABILITY_FOR_TARGET,
-        standardise_input=True,
-        input_stats=INPUT_STATS,
-        independently_center_inputs=INDEPENDENTLY_CENTER_INPUTS,
-        subsample_target=SUBSAMPLE_TARGET,
-        input_padding=INPUT_PADDING
-    )
+    # real_appliance_source1 = RealApplianceSource(
+    #     logger=logger,
+    #     filename=UKDALE_FILENAME,
+    #     appliances=[
+    #         TARGET_APPLIANCE,
+    #         ['fridge freezer', 'fridge', 'freezer'],
+    #         'dish washer',
+    #         'kettle',
+    #         ['washer dryer', 'washing machine']
+    #     ],
+    #     max_appliance_powers=[MAX_TARGET_POWER, 300, 2500, 2600, 2400],
+    #     on_power_thresholds=[ON_POWER_THRESHOLD] + [10] * 4,
+    #     min_on_durations=[MIN_ON_DURATION, 60, 1800, 12, 1800],
+    #     min_off_durations=[MIN_OFF_DURATION, 12, 1800, 12, 600],
+    #     divide_input_by_max_input_power=False,
+    #     window_per_building=WINDOW_PER_BUILDING,
+    #     seq_length=SEQ_LENGTH,
+    #     output_one_appliance=True,
+    #     train_buildings=TRAIN_BUILDINGS,
+    #     validation_buildings=VALIDATION_BUILDINGS,
+    #     n_seq_per_batch=N_SEQ_PER_BATCH,
+    #     skip_probability=0.75,
+    #     skip_probability_for_first_appliance=SKIP_PROBABILITY_FOR_TARGET,
+    #     standardise_input=True,
+    #     input_stats=INPUT_STATS,
+    #     independently_center_inputs=INDEPENDENTLY_CENTER_INPUTS,
+    #     subsample_target=SUBSAMPLE_TARGET,
+    #     input_padding=INPUT_PADDING
+    # )
 
-    same_location_source1 = SameLocation(
-        logger=logger,
-        filename=UKDALE_FILENAME,
-        target_appliance=TARGET_APPLIANCE,
-        window_per_building=WINDOW_PER_BUILDING,
-        seq_length=SEQ_LENGTH,
-        train_buildings=TRAIN_BUILDINGS,
-        validation_buildings=VALIDATION_BUILDINGS,
-        n_seq_per_batch=N_SEQ_PER_BATCH,
-        skip_probability=SKIP_PROBABILITY_FOR_TARGET,
-        standardise_input=True,
-        offset_probability=1,
-        divide_target_by=MAX_TARGET_POWER,
-        input_stats=INPUT_STATS,
-        independently_center_inputs=INDEPENDENTLY_CENTER_INPUTS,
-        on_power_threshold=ON_POWER_THRESHOLD,
-        min_on_duration=MIN_ON_DURATION,
-        min_off_duration=MIN_OFF_DURATION,
-        include_all=True,
-        allow_incomplete=True,
-        subsample_target=SUBSAMPLE_TARGET,
-        input_padding=INPUT_PADDING
-    )
+    # same_location_source1 = SameLocation(
+    #     logger=logger,
+    #     filename=UKDALE_FILENAME,
+    #     target_appliance=TARGET_APPLIANCE,
+    #     window_per_building=WINDOW_PER_BUILDING,
+    #     seq_length=SEQ_LENGTH,
+    #     train_buildings=TRAIN_BUILDINGS,
+    #     validation_buildings=VALIDATION_BUILDINGS,
+    #     n_seq_per_batch=N_SEQ_PER_BATCH,
+    #     skip_probability=SKIP_PROBABILITY_FOR_TARGET,
+    #     standardise_input=True,
+    #     offset_probability=1,
+    #     divide_target_by=MAX_TARGET_POWER,
+    #     input_stats=INPUT_STATS,
+    #     independently_center_inputs=INDEPENDENTLY_CENTER_INPUTS,
+    #     on_power_threshold=ON_POWER_THRESHOLD,
+    #     min_on_duration=MIN_ON_DURATION,
+    #     min_off_duration=MIN_OFF_DURATION,
+    #     include_all=True,
+    #     allow_incomplete=True,
+    #     subsample_target=SUBSAMPLE_TARGET,
+    #     input_padding=INPUT_PADDING
+    # )
 
-    multi_source = MultiSource(
-        sources=[
-            {
-                'source': real_appliance_source1,
-                'train_probability': 0.5,
-                'validation_probability': 0
-            },
-            {
-                'source': same_location_source1,
-                'train_probability': 0.5,
-                'validation_probability': 1
-            }
-        ],
-        standardisation_source=same_location_source1
-    )
+    # multi_source = MultiSource(
+    #     sources=[
+    #         {
+    #             'source': real_appliance_source1,
+    #             'train_probability': 0.5,
+    #             'validation_probability': 0
+    #         },
+    #         {
+    #             'source': same_location_source1,
+    #             'train_probability': 0.5,
+    #             'validation_probability': 1
+    #         }
+    #     ],
+    #     standardisation_source=same_location_source1
+    # )
 
     net_dict_copy = deepcopy(net_dict)
     net_dict_copy.update(dict(
@@ -214,10 +214,10 @@ def exp_a(name):
             n_seq_to_plot=32,
             n_training_examples_to_plot=16
         ),
-        learning_rate=1e-5,
+        learning_rate=1e-7,
         learning_rate_changes_by_iteration={
-            1000: 1e-6,
-            10000: 1e-7
+            1000: 1e-8,
+            10000: 1e-9
         },
         layers_config=[
             {
@@ -230,12 +230,14 @@ def exp_a(name):
                 'num_units': 128,
                 'merge_mode': 'concatenate',
                 'grad_clipping': 1.0
+#                'gradient_steps': 200
             },
             {
                 'type': BLSTMLayer,
                 'num_units': 256,
                 'merge_mode': 'concatenate',
                 'grad_clipping': 1.0
+#                'gradient_steps': 200                
             },
             {
                 'type': DenseLayer,
